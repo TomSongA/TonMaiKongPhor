c842723cd941f8d25b109c53227c481629aa7693
cooldog98 <patthiaon.p@ku.th>
2026-04-02 13:32:20 +0700

    remove .next from tracking

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nkm4UldM69NY9tN6dhxYMXrkPWrX2vsaX23CAji9QAo=\"\n}"
--- frontend/.next/dev/static/chunks/src_app_page_jsx_13-g9dz._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_recharts_es6_util_0y-_diw._.js","static/chunks/node_modules_recharts_es6_state_0pp-cy4._.js","static/chunks/node_modules_recharts_es6_component_130w5qe._.js","static/chunks/node_modules_recharts_es6_cartesian_10dv~kv._.js","static/chunks/node_modules_recharts_es6_02ojrfc._.js","static/chunks/node_modules_06w8eo1._.js","static/chunks/src_0gey~vp._.js","static/chunks/src_0t-lma7._.css"],
-    source: "dynamic"
-});
